a42ed4239d2ef2276bed39f3957f74ddc1bf2d76
BURAK SONMEZ <42543196+xbsonmez@users.noreply.github.com>
2022-05-19 17:30:59 +0300

    Updated Yarn lockfile

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    next();
+  });
+
 app.use(bodyParser.json());
 app.use("/feedbacks", feedbacksRouter);
 
