94fcbe9f7a7cd5bab69f27ccd6f79dded15a9c27
BURAK SONMEZ <42543196+xbsonmez@users.noreply.github.com>
2022-05-19 14:10:29 +0300

    Changed port

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ mongoose.connect(
     }
 );
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     console.log('Server is running on port 5000');
 });
 
