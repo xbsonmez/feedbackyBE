d86c2c4fb69815066bd710accb6d6fc2d388e381
BURAK SONMEZ <42543196+xbsonmez@users.noreply.github.com>
2022-05-19 17:44:51 +0300

    Updated cors

--- routes/feedbacks.js
+++ routes/feedbacks.js
@@ -5,6 +5,7 @@ const Feedbacks = require('../models/Feedbacks');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
     Feedbacks.find()
     .then(feedbacks => {
         res.json(feedbacks);
@@ -15,6 +16,7 @@ router.get('/', (req, res) => {
 });
 
 router.post("/", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
     const feedback = new Feedbacks({
         feedbacks: req.body.feedbacks
     });
@@ -25,6 +27,7 @@ router.post("/", (req, res) => {
 });
 
 router.delete("/:id", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
     Feedbacks.findOneAndDelete({ _id: req.params.id }, (err, feedback) => {
         if (err) {
             res.send(err);
